5dc214891442f037914612a8e0990bd86a755566
AdityaP183 <adityaprasad1828@gmail.com>
2026-06-20 21:03:17 +0530

    feat: setup shadcn and application foundation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Oxanium, Roboto } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const oxaniumHeading = Oxanium({
+    subsets: ["latin"],
+    variable: "--font-heading",
+});
+
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -25,7 +33,15 @@ export default function RootLayout({
     return (
         <html
             lang="en"
-            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+            className={cn(
+                "h-full",
+                "antialiased",
+                geistSans.variable,
+                geistMono.variable,
+                "font-sans",
+                roboto.variable,
+                oxaniumHeading.variable,
+            )}
         >
             <body className="min-h-full flex flex-col">{children}</body>
         </html>
